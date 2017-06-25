ba9401815331f3c7c7b8f4e73b7b9d49b301e271
cnlisn <954375802@qq.com>
2017-06-25 14:10:46 +0800

    自定义 自动换行的view添加图片点击事件

--- app/src/main/java/com/lisn/sg/WordWrapView.java
+++ app/src/main/java/com/lisn/sg/WordWrapView.java
@@ -1,14 +1,20 @@
 package com.lisn.sg;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.lisn.sg.View.FixGridLayout;
 
@@ -31,13 +37,29 @@ public class WordWrapView extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         initToolBar(toolbar,"自定义，自动换行view",true);
 
-        FixGridLayout fixGridLayout = (FixGridLayout) findViewById(R.id.fgl);
+        FixGridLayout fixGridLayout = (FixGridLayout) findViewById(R.id.fgl1);
         fixGridLayout.setmCellHeight(30);
         fixGridLayout.setmCellWidth(100);
         for (int i = 0; i < 17; i++) {
             CheckBox box = new CheckBox(WordWrapView.this);
             box.setText("第"+i+"个");
             fixGridLayout.addView(box);
+        }
+        FixGridLayout fixGridLayout2 = (FixGridLayout) findViewById(R.id.fgl2);
+        fixGridLayout2.setmCellHeight(100);
+        fixGridLayout2.setmCellWidth(100);
+        for (int i = 0; i < 12; i++) {
+            ImageView imageView = new ImageView(mContext);
+            Drawable drawable = getResources().getDrawable(R.mipmap.ic_launcher);
+            imageView.setBackground(drawable);
+            final int finalI = i;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(mContext, "点击了第"+ finalI +"个图片", Toast.LENGTH_SHORT).show();
+                }
+            });
+            fixGridLayout2.addView(imageView);
         }
     }
 
